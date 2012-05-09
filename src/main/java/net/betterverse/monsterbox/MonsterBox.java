2198c9eb88c0e93fd5db62110bd3999d0c7e4b1e
aPunch <theapunch@yahoo.com>
2012-05-09 15:41:16 -0500

    Players can now edit the mob of a spawner. Block metadata/persistence is not yet implemented properly.

--- src/main/java/net/betterverse/monsterbox/MonsterBox.java
+++ src/main/java/net/betterverse/monsterbox/MonsterBox.java
@@ -1,15 +1,61 @@
 package net.betterverse.monsterbox;
 
-import java.util.logging.Level;
+import java.util.HashMap;
+import java.util.Map;
 
-import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.CreatureSpawner;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.metadata.FixedMetadataValue;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class MonsterBox extends JavaPlugin {
+public class MonsterBox extends JavaPlugin implements Listener {
+    private final Map<String, SpawnerEditor> editing = new HashMap<String, SpawnerEditor>();
 
     @Override
-    public void onEnable() {
-        log(toString() + " enabled.");
+    public boolean onCommand(CommandSender sender, Command cmd, String cmdLabel, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("You must be in game to use /mbox!");
+            return true;
+        }
+
+        Player player = (Player) sender;
+        if (args.length == 1 && args[0].equals("set")) {
+            if (!player.hasPermission("monsterbox.set")) {
+                player.sendMessage(ChatColor.RED + "You do not have permission.");
+                return true;
+            }
+            // Toggle player's editor
+            if (editing.containsKey(player.getName())) {
+                editing.remove(player.getName());
+                player.sendMessage(ChatColor.GREEN + "Exited the spawner editor.");
+            } else {
+                SpawnerEditor editor = new SpawnerEditor(player);
+                if (editor.canEdit()) {
+                    editing.put(player.getName(), editor);
+                    player.sendMessage(ChatColor.GREEN
+                            + "Right-click a mob spawner to change the mob that it will spawn.");
+                } else {
+                    player.sendMessage(ChatColor.RED + "You do not have permission.");
+                }
+            }
+        } else {
+            player.sendMessage(ChatColor.RED + "Invalid command. /mbox set");
+        }
+
+        return true;
     }
 
     @Override
@@ -17,11 +63,74 @@ public class MonsterBox extends JavaPlugin {
         log(toString() + " disabled.");
     }
 
-    public void log(Level level, String message) {
-        Bukkit.getLogger().log(level, "[MonsterBox] " + message);
+    @Override
+    public void onEnable() {
+        getConfig().options().copyDefaults(true);
+        saveConfig();
+
+        getServer().getPluginManager().registerEvents(this, this);
+
+        log(toString() + " enabled.");
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder(getDescription().getName() + " v" + getDescription().getVersion()
+                + " [Written by: ");
+        for (int i = 0; i < getDescription().getAuthors().size(); i++) {
+            builder.append(getDescription().getAuthors().get(i)
+                    + (i + 1 != getDescription().getAuthors().size() ? ", " : ""));
+        }
+        builder.append("]");
+
+        return builder.toString();
+    }
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent event) {
+        if (!event.isCancelled() && event.getBlock().getType() == Material.MOB_SPAWNER) {
+            if (!event.getPlayer().hasPermission("monsterbox.break")) {
+                event.setCancelled(true);
+                event.getPlayer().sendMessage(ChatColor.RED + "You do not have permission to break monster spawners.");
+                return;
+            }
+
+            Block block = event.getBlock();
+            // Store its metadata
+            block.setMetadata("mob", new FixedMetadataValue(this, ((CreatureSpawner) block.getState()).getSpawnedType()
+                    .name()));
+            // Drop item
+            block.getWorld().dropItemNaturally(block.getLocation(), new ItemStack(Material.MOB_SPAWNER, 1));
+        }
+    }
+
+    @EventHandler
+    public void onBlockPlace(BlockPlaceEvent event) {
+        if (!event.isCancelled() && event.getBlock().getType() == Material.MOB_SPAWNER
+                && event.getBlock().hasMetadata("mob")) {
+            // Set spawner's mob type based on its metadata
+            Block block = event.getBlock();
+            ((CreatureSpawner) block).setSpawnedType(EntityType.fromName(block.getMetadata("mob").get(0).asString()));
+        }
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        // Exit editor if player quits the game
+        if (editing.containsKey(event.getPlayer().getName())) {
+            editing.remove(event.getPlayer().getName());
+        }
+    }
+
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        Player player = event.getPlayer();
+        if (editing.containsKey(player.getName())) {
+            editing.get(player.getName()).onInteract(event);
+        }
     }
 
     public void log(String message) {
-        log(Level.INFO, message);
+        getServer().getLogger().info("[MonsterBox] " + message);
     }
 }
---
+++ src/main/java/net/betterverse/monsterbox/SpawnerEditor.java
@@ -0,0 +1,87 @@
+package net.betterverse.monsterbox;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.CreatureSpawner;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+public class SpawnerEditor {
+    private final List<EntityType> mobs = new ArrayList<EntityType>();
+    private final Player player;
+    private Block editing;
+    private int currentMob;
+
+    public SpawnerEditor(Player player) {
+        this.player = player;
+
+        calculatePossibleMobs();
+    }
+
+    public boolean canEdit() {
+        return mobs.size() > 0;
+    }
+
+    public void onInteract(PlayerInteractEvent event) {
+        switch (event.getAction()) {
+        case LEFT_CLICK_BLOCK:
+            // Don't allow player to cycle through mobs until it has right-clicked a block
+            if (editing == null) {
+                player.sendMessage(ChatColor.GREEN + "Right-click a mob spawner to change the mob that it will spawn.");
+                return;
+            }
+            if (++currentMob > mobs.size() - 1) {
+                currentMob = 0;
+            }
+            // Cycle through possible mobs based on the player's permissions
+            player.sendMessage(ChatColor.GREEN + "Changed mob to " + ChatColor.YELLOW
+                    + mobs.get(currentMob).name().toLowerCase().replace('_', ' ') + ChatColor.GREEN
+                    + ". Right-click to confirm.");
+            break;
+        case RIGHT_CLICK_BLOCK:
+            if (event.getClickedBlock().getType() == Material.MOB_SPAWNER) {
+                if (editing == null) {
+                    // Set the edited block
+                    editing = event.getClickedBlock();
+                    player.sendMessage(ChatColor.GREEN + "Mob spawner was set to block at location: "
+                            + ChatColor.YELLOW + editing.getX() + ChatColor.GREEN + ", " + ChatColor.YELLOW
+                            + editing.getY() + ChatColor.GREEN + ", " + ChatColor.YELLOW + editing.getZ()
+                            + ChatColor.GREEN + " in world " + ChatColor.YELLOW + editing.getWorld().getName()
+                            + ChatColor.GREEN + ". Left-click the mob spawner to cycle through mobs.");
+                } else {
+                    if (editing.equals(event.getClickedBlock())) {
+                        EntityType mob = mobs.get(currentMob);
+                        CreatureSpawner spawned = (CreatureSpawner) event.getClickedBlock().getState();
+                        spawned.setSpawnedType(mob);
+                        // Update the client
+                        spawned.update();
+
+                        player.sendMessage(ChatColor.GREEN + "This mob spawner now will spawn " + ChatColor.YELLOW
+                                + mob.name().toLowerCase().replace('_', ' ') + ChatColor.GREEN + ". Enter "
+                                + ChatColor.YELLOW + "/mbox set" + ChatColor.GREEN + " again to exit the editor.");
+                    } else {
+                        // The player is already editing a different block
+                        player.sendMessage(ChatColor.RED + "That is not the correct block!");
+                    }
+                }
+            }
+        }
+    }
+
+    private void calculatePossibleMobs() {
+        for (EntityType entity : EntityType.values()) {
+            // Only check LivingEntities, don't include players
+            if (!entity.isAlive() || entity.getEntityClass().equals(Player.class)) {
+                continue;
+            }
+            if (player.hasPermission("monsterbox.spawn." + entity.name().toLowerCase().replace("_", ""))) {
+                mobs.add(entity);
+            }
+        }
+    }
+}
